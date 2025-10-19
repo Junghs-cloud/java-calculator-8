6adbf7db79c212ee44d588011b71c9cba6113625
Junghs-cloud <rabbit4935@gmail.com>
2025-10-19 11:13:05 +0900

    Add: 기본 구분자 사용했을 때 덧셈 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import java.util.*;
+import java.util.stream.IntStream;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String userInput = readLine();
+        String basicDelimiterRegex = "[,:]";
+        String[] operands = userInput.split(basicDelimiterRegex);
+        int sum  = Arrays.stream(operands).mapToInt(Integer::parseInt).sum();
+        System.out.println("결과 : " + sum);
     }
+
 }
