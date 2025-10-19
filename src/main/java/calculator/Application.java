6d9c1e989e89cb05c6353df5b0f727c2a475bc0c
Junghs-cloud <rabbit4935@gmail.com>
2025-10-19 11:42:25 +0900

    Add: 잘못된 값 입력했을 때 오류 출력하기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,10 +9,32 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInput = readLine();
-        String basicDelimiterRegex = "[,:]";
+        String basicDelimiterRegex = "[,:]+";
         String[] operands = userInput.split(basicDelimiterRegex);
-        int sum  = Arrays.stream(operands).mapToInt(Integer::parseInt).sum();
-        System.out.println("결과 : " + sum);
+        if (userInput.isEmpty()) {
+            System.out.println("결과 : 0");
+        }
+        else {
+            Vector<Integer> operandValues = getOperandValues(operands);
+            int sum  = operandValues.stream().mapToInt(Integer::intValue).sum();
+            System.out.println("결과 : " + sum);
+        }
     }
 
+    public static Vector<Integer> getOperandValues(String[] operands) {
+        Vector<Integer> operandValues = new Vector<>();
+        try {
+            for (String operand: operands) {
+                int value = Integer.parseInt(operand);
+                if (value <= 0) {
+                    throw new IllegalArgumentException();
+                }
+                operandValues.add(value);
+            }
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        return operandValues;
+    }
 }
